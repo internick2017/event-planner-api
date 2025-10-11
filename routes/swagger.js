12480ebb4eafc9b24ea3087a2a1a17c645754d21
Nick Granados <53047332+internick2017@users.noreply.github.com>
2025-10-11 17:42:38 -0300

    Update Swagger configuration to dynamically set host and scheme; enhance API documentation with tags

--- routes/swagger.js
+++ routes/swagger.js
@@ -3,6 +3,23 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
 
 router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+router.get('/api-docs', (req, res) => {
+  // Dynamically set the host based on the request
+  const dynamicSwaggerDoc = { ...swaggerDocument };
+  
+  // Determine host from request
+  const host = req.get('host');
+  const protocol = req.protocol;
+  
+  dynamicSwaggerDoc.host = host;
+  dynamicSwaggerDoc.schemes = [protocol];
+  
+  // Update OAuth authorization URL
+  if (dynamicSwaggerDoc.securityDefinitions && dynamicSwaggerDoc.securityDefinitions.OAuth2) {
+    dynamicSwaggerDoc.securityDefinitions.OAuth2.authorizationUrl = `${protocol}://${host}/auth/google`;
+  }
+  
+  return swaggerUi.setup(dynamicSwaggerDoc)(req, res);
+});
 
 module.exports = router;
